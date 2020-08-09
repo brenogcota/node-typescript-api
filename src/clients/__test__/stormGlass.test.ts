e5eff21923295a8bd1193a32cc597ca241b8d25e
brenogcota <brenocota4@gmail.com>
2020-08-09 14:57:33 -0300

    create client for fetch stormglass api

---
+++ src/clients/__test__/stormGlass.test.ts
@@ -0,0 +1,19 @@
+import { StormGlass } from '@src/clients/stormGlass';
+import axios from 'axios';
+import  stormglass_weather_3_hours  from '@test/fixtures/stormglass_weather_3_hours.json';
+import stormglass_normalized_response_3_hours from '@test/fixtures/stormglass_normalized_response_3_hours.json';
+
+jest.mock('axios');
+
+describe('StoreGlass client', () => {
+    it('should return the normalized forecast from the StoreGlass service', async ()=>{
+        const lat = -33.7865412;
+        const lng = 151.2888394;
+
+        axios.get = jest.fn().mockResolvedValue(stormglass_weather_3_hours);
+
+        const stormGlass = new StormGlass(axios);
+        const response = await stormGlass.fetchPoints(lat, lng);
+        expect(response).toEqual(stormglass_normalized_response_3_hours);
+    });
+});
